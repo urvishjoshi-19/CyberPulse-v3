91a7483e740c753c26014deaf9d939ec40e6d4af
urvishjoshi-19 <urvishwork19@gmail.com>
2025-05-05 15:15:35 +0530

    Fix: install Python dependenciesr

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: pip install -r python-requirements.txt && npm start
---
+++ _setup.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Install Node.js dependencies
+npm install
+
+# Install Python dependencies
+pip install requests beautifulsoup4
+
+echo "All dependenAcies installed successfully!"
